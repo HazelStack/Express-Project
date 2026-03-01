21bb60054330f76ad8023130559e6160333064bb
HazelArevalo <arevalo.hja@gmail.com>
2026-03-01 12:10:56 -0800

    Fix API URLs for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = "/api"; // ← fixed for deployed site
+
 export async function getQuestionsByCategory(categoryID) {
   const res = await axios.get(`${API_BASE}/questions/category/${categoryID}`);
   return res.data;
 }
+
 export async function login(username, password) {
-  const res = await axios.post(`${API_BASE}/login`, { username, password });
+  const res = await axios.post(`${API_BASE}/users/login`, { username, password });
   return res.data;
 }
 
 export async function register(username, password, email) {
-  const res = await axios.post(`${API_BASE}/register`, { username, password, email });
+  const res = await axios.post(`${API_BASE}/users/register`, { username, password, email });
   return res.data;
 }
 
@@ -29,6 +31,7 @@ export async function postAnswer(questionID, content, userID) {
   const res = await axios.post(`${API_BASE}/questions/${questionID}/answers`, { content, userID });
   return res.data;
 }
+
 export async function getCategories() {
   const res = await axios.get(`${API_BASE}/categories`);
   return res.data;
