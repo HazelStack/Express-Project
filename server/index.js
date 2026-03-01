62c53fa6f1d262bfdc2da693eef70c01c7c16a5d
HazelArevalo <arevalo.hja@gmail.com>
2026-02-28 21:58:23 -0800

    Prepare server for production: serve React build in index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,33 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import usersRouter from "./Routers/usersRouter.js";
 import questionsRouter from "./Routers/questionsRouter.js";
-import categoriesRouter from "./Routers/categoriesRouter.js"
+import categoriesRouter from "./Routers/categoriesRouter.js";
 
 const server = express();
+const PORT = process.env.PORT || 4000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 server.use(cors());
 server.use(express.json());
 
-server.use("/api", usersRouter);
+
+server.use("/api/users", usersRouter);
 server.use("/api/questions", questionsRouter);
 server.use("/api/categories", categoriesRouter);
 
-server.get("/", (req, res) => {
-  res.send("Server is running");
+
+server.use(express.static(path.join(__dirname, "client/build")));
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-server.listen(4000, () => {
-  console.log("Server listening on port 4000");
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
